30615383826d64b8641949f553c7b42116a06861
swkc95 <161343327+swkc95@users.noreply.github.com>
2024-03-12 16:47:54 +0100

    Update driver.py - grid support

--- features/fixtures/driver.py
+++ features/fixtures/driver.py
@@ -1,20 +1,19 @@
-import os
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
 
 
-# def get_remote_driver(context, executor):
-#     os.environ['DISPLAY'] = ':0'
-#     if context.used_browser.lower() == "chrome":
-#         options = webdriver.ChromeOptions()
-#         options.add_argument("--window-size=1920,1080")
-#         options.add_argument('--no-sandbox')
-#         options.add_argument('--disable-dev-shm-usage')
-#         driver = webdriver.Remote(command_executor=executor, options=options)
-#     else:
-#         raise ValueError("Invalid used_browser")
-#     return driver
+def get_remote_driver(context, executor):
+    if context.used_browser.lower() == "chrome":
+        options = webdriver.ChromeOptions()
+        options.add_argument("--lang=en-us")
+        options.add_argument("--window-size=1920,1080")
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+        driver = webdriver.Remote(command_executor=executor, options=options)
+    else:
+        raise ValueError("Invalid used_browser")
+    return driver
 
 
 def get_local_driver(context):
